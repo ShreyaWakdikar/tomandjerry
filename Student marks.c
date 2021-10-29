3e74da4fd259acca30c12a4e05304ec64a6834bf
ShreyaWakdikar <93387875+ShreyaWakdikar@users.noreply.github.com>
2021-10-29 19:57:17 +0530

    Created student marks.c
    This file prints grade as per student marks on the screen.

---
+++ Student marks.c
@@ -0,0 +1,23 @@
+#include<studio.h>
+int main()
+{
+char marks;
+{
+printf("Enter the marks");
+}
+if(100<marks>85)
+{ printf("Grade A")
+}
+else
+if(84<marks>70)
+{printf("Grade B");
+}
+else if(69<marks>55)
+{ printf("Grade C");
+}
+else if(54<marks>40)
+{printf("Grade D");
+} else
+{printf("Grade F");
+}
+}
